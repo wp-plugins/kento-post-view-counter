b5600ba00ececc41ede16e599b8d092eca24e2da
kentothemes <kentothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-13 05:28:18 +0000

    update 1.8
    git-svn-id: https://plugins.svn.wordpress.org/kento-post-view-counter/tags/1.8@856937 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kento-pvc-admin.php
+++ kento-pvc-admin.php
@@ -1,164 +1,87 @@
+<?php
+		if($_POST['kentopvc_hidden'] == 'Y') {
+			//Form data sent
+			
+
+			$kento_pvc_hide = $_POST['kento_pvc_hide'];
+			update_option('kento_pvc_hide', $kento_pvc_hide);
+		
+			$kento_pvc_posttype = $_POST['kento_pvc_posttype'];
+			update_option('kento_pvc_posttype', $kento_pvc_posttype);			
+
+			?>
+			<div class="updated"><p><strong><?php _e('Changes Saved.' ); ?></strong></p>
+            </div>
+
+			<?php
+		} else {
+			//Normal page display
+			$kento_pvc_hide = get_option( 'kento_pvc_hide' );
+			$kento_pvc_posttype = get_option( 'kento_pvc_posttype' );
+		}
+
+?>
+
 
 <div class="wrap">
-<div class="kento-pvc-by-viewed">
-<h3>Top Viewed Post</h3>
+	<div id="icon-tools" class="icon32"><br></div><?php echo "<h2>".__('Kento Post View Counter Settings')."</h2>";?>
+		<form  method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+	<input type="hidden" name="kentopvc_hidden" value="Y">
+        <?php settings_fields( 'kento_pvc_plugin_options' );
+				do_settings_sections( 'kento_pvc_plugin_options' );
+			
+		?>
+
+<table class="form-table">
+               
+	<tr valign="top">
+		<th scope="row">Hide Count on Post</th>
+		<td style="vertical-align:middle;">
         
-    <?php
-	global $wpdb;
-	$table = $wpdb->prefix . "kento_pvc";
-	
-	$result = $wpdb->get_results("SELECT * FROM $table ORDER BY count DESC LIMIT 12", ARRAY_A);
-	$total_rows = $wpdb->num_rows;
-	
-	echo "<table class='kento-pvc-top-post'>";
-		echo "<tr><td>";
-		echo "<strong>View Count</strong>";
-		echo "</td>";
-		echo "<td>";
-		echo "<strong>Post Title</strong>";
-		echo "</td>";		
-		echo "</tr>";	
+         
+                           
+                     <input type="checkbox" name="kento_pvc_hide" id="kento-pvc-hide"  value ="1" <?php if (  $kento_pvc_hide==1 ) echo "checked"; ?>> ** this will hide post count on single content but still can record data. 
+		</td>
+	</tr>
 
-	for($i=0;$i<$total_rows;$i++)
-		{
-		echo "<tr><td>";
-		echo $view_count = $result[$i]['count']."<br />";
-		echo "</td>";
-		echo "<td class='post-title'>";
-		$postid= $result[$i]['postid']."<br />";
-		
-		echo "<a href=".get_permalink( $postid ).">";
-		echo get_the_title($postid);		
-		echo "</a>";
-		echo "</td>";		
-		echo "</tr>";
+	<tr valign="top">
+		<th scope="row">Trace These Post Type Only</th>
+		<td style="vertical-align:middle;">
+        
+<?php
 
-		}
-	echo "</table>";
-	
-	?>    
+$post_types = get_post_types( '', 'names' ); 
+
+foreach ( $post_types as $post_type ) {
+
+   echo '<label for="kento-pvc-posttype['.$post_type.']"><input type="checkbox" name="kento_pvc_posttype['.$post_type.']" id="kento-pvc-posttype['.$post_type.']"  value ="1" ' ?> 
+   <?php if ( isset( $kento_pvc_posttype[$post_type] ) ) echo "checked"; ?>
+   <?php echo' >'. $post_type.'</label><br />' ;
+}
+?>
+                           
+
+		</td>
+	</tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</table>
+                <p class="submit">
+                    <input class="button button-primary" type="submit" name="Submit" value="<?php _e('Save Changes' ) ?>" />
+                </p>
+		</form>
+
+   
 </div>
-<div class="kento-pvc-by-date">
-<h3>Top Viewed By Date</h3>
-Select Date
-
-<input type="text" id="kento-pvc-date" name="kento_pvc_date" value="<?php echo date("Y-m-d");?>"/><br />
-<div class="kento-pvc-by-date-submit">Submit</div>
-	<script>
-    jQuery(document).ready(function() {
-        jQuery('#kento-pvc-date').datepicker({
-            dateFormat : 'yy-mm-dd'
-        });
-    });
-    </script>
-
-<div id="kento-pvc-by-date-result">
-<br />
-    <?php
-	global $wpdb;
-	$table = $wpdb->prefix . "kento_pvc_info";
-	$today = date("Y-m-d");
-	$result = $wpdb->get_results("SELECT * FROM $table WHERE date = CURDATE() ORDER BY count DESC LIMIT 12", ARRAY_A);
-	$total_rows = $wpdb->num_rows;
-	
-	
-	echo "<table class='kento-pvc-top-post'>";
-		echo "<tr><td>";
-		echo "<strong>View Count</strong>";
-		echo "</td>";
-		echo "<td>";
-		echo "<strong>Post Title</strong>";
-		echo "</td>";		
-		echo "</tr>";	
-	$i=0;
-	while($total_rows>$i)
-		{
-		echo "<tr><td>";
-		echo $view_count = $result[$i]['count']."<br />";
-		echo "</td>";
-		echo "<td class='post-title'>";
-		$postid= $result[$i]['postid']."<br />";
-		
-		echo "<a href=".get_permalink( $postid ).">";
-		echo get_the_title($postid);		
-		echo "</a>";
-		echo "</td>";		
-		echo "</tr>";
-		
-		
-		
-		$i++;
-		}
-	echo "</table>";
-	
-	?>  
-</div>  <!--kento-pvc-by-date-result -->
-    
-      
-</div>    <!--kento-pvc-by-date -->
-
-
-<div class="kento-pvc-by-city">
-    <h3>Recent Viewed by City</h3>
-
-    <?php
-	global $wpdb;
-	$table = $wpdb->prefix . "kento_pvc_city";
-	
-	$result = $wpdb->get_results("SELECT * FROM $table ORDER BY city DESC LIMIT 12", ARRAY_A);
-	$total_rows = $wpdb->num_rows;
-	
-	echo "<table class='kento-pvc-top-post'>";
-		echo "<tr><td>";
-		echo "<strong>City</strong>";
-		echo "</td>";
-		echo "<td>";
-		echo "<strong>Country</strong>";
-		echo "</td>";
-		echo "<td>";
-		echo "<strong>Post Title</strong>";
-		echo "</td>";	
-				
-		echo "</tr>";	
-	$i=0;
-	while($total_rows>$i)
-		{
-		echo "<tr><td>";
-		echo $view_count = $result[$i]['city'];
-		echo "</td>";
-		echo "<td>";
-		$view_city= $result[$i]['country'];
-		echo $view_city;
-		echo "</td>";
-		echo "<td class='post-title'>";
-		$postid= $result[$i]['postid'];
-		echo "<a href=".get_permalink( $postid ).">";
-		echo get_the_title($postid);		
-		echo "</a>";
-		echo "<span class='kento-pvc-by-city-details' postid='".$postid."'>  Deatils &raquo;</span>";
-		
-		echo "</td>";
-		echo "</tr>";
-
-		$i++;
-		}
-	echo "</table>";
-	
-	?>        
-        
-        
-        </div> <!--kento-pvc-by-city -->
-        
-        
-    <div class="kento-pvc-by-city-post">
-    <h3>Recent Viewed Post by City</h3>
-    <div id="kento-pvc-by-city-post-result">
-    Please click 'details' left to post title on "Recent Viewed by City" box
-    </div> <!--kento-pvc-by-city-post -->   
-        
-        
-        
-        </div> <!--kento-pvc-by-city-post -->    
-        
-        
-</div> <!--wrap -->
--- readme.txt
+++ readme.txt
@@ -3,12 +3,12 @@ Contributors: hasanrang05
 Donate link: 
 Tags:  Kento Post View Counter, post view by city, post view by country, page view counter, view counter, Post View Counter
 Requires at least: 3.7
-Tested up to: 3.8
-Stable tag: 1.7
+Tested up to: 3.8.1
+Stable tag: 1.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Post View Counter by City or Country or by Date 
+Post View Counter by City or Country or by Date.
 
 == Description ==
 
@@ -22,6 +22,12 @@ Live Preview: http://kentothemes.com/demo/kento-pvc/hello-world/
 
 Plugin Features
 
+* Top 10 Country list by visitors.(New)
+* Top 10 refere site list.
+* Display via shortcodes only today's coount or total count or both(New)
+* Add refer site where visitors come from(new).
+* Filter post type to trace count(new).
+* hide stats on content but still can trace view count(new).
 * Total Post View Count.
 * Total Post View Count Daily.
 * Top Viewed Post List on Admin.
@@ -38,6 +44,43 @@ Plugin Features
 2. Go your Pluings setting via WordPress Dashboard and activate it.
 3. After Activate Visit your Single Post.
 4. and after activate you will see menu on dashboard "Kento PVC" will get stats.
+<br /><br />
+
+<strong>How to use shortcodes ?</strong><br />
+To display views count manually via shortcodes you can display only today's view count or total view count on single post.
+Before use shotcodes you need to <strong>"Hide Count on Post"</strong> checked and <strong>"Trace These Post Type Only"</strong> checked for custom post type<br /><br />
+
+And use these shortcode as your need<br />
+
+<strong>Display whitout customize</strong><br />
+`[kpvc_single]` <br />
+`<?php echo do_shortcode("[kpvc_single]"); ?>` Anywhere on single.php or custom post type single.php
+Also same way you can php code for bellow shortcodes<br />
+<strong>Display only today views count or hide total views count.</strong><br />
+`[kpvc_single total="no"]`<br />
+
+<strong>Display only total views count or hide today views count.</strong><br />
+`[kpvc_single today="no"]`<br />
+
+<strong>Display Custom text for today or total views count</strong><br />
+`[kpvc_single today_text="hello today" total_text="hello total"]`<br />
+
+
+
+<br />
+
+<strong>How to use shortcodes in loop or excerpt ?</strong><br />
+To display views count manually via shortcodes in loop or excerpt mode you can use shortcode bellow<br />
+
+<strong>Display whitout customize</strong><br />
+`<?php do_shortcode('[kpvc_loop post_id='.get_the_ID().']'); ?>`
+
+<strong>Display only total views count or hide today views count.</strong><br />
+`<?php do_shortcode('[kpvc_loop post_id='.get_the_ID().' today="no" ] '); ?>`<br />
+same way you can hide total count by adding today="no"<br />
+
+<strong>Display Custom text for today or total views count</strong><br />
+`<?php do_shortcode('[kpvc_loop post_id='.get_the_ID().' today_text="hello today" total_text="hello total" ] '); ?>`<br />
 
 
 == Screenshots ==
@@ -45,10 +88,42 @@ Plugin Features
 1. View Counter on post.
 2. Admin - Top viewed post and Top Viewed By Date.
 3. Admin - Recent Viewed by City and Recent Viewed Post by City with time.
+4. Settings page.
+
+
 
 
 
 == Changelog ==
+
+= 1.8 =
+* Added Top 10 Country on Stats Page.
+
+= 1.7 =
+* Display Post view Counter on loop or excerpt mode.
+
+= 1.6 =
+* Add Top 10 refer site list.
+
+= 1.5 =
+* Add Shortcode support to display single post.
+
+= 1.4 =
+* Added refer site to details report.
+* Added send us feedback link.
+
+= 1.3 =
+* Added icon for total and daily counter.
+* Added New Page for setting.
+* Filter post type.
+* Simply hid on post stats but still can trcae.
+* Added different stats page on admin.
+
+= 1.2 =
+* Fixing problem with Pagination
+
+= 1.1 =
+* Added Paginate "Recent Viewed By City & Country
 
 = 1.0 =
 * Initial release
