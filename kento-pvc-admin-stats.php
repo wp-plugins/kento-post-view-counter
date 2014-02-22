b14236aefc0dd0fc0572ef0d16217707040eccfb
kentothemes <kentothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-22 10:56:12 +0000

    kento-pvc-admin-stats.php update
    git-svn-id: https://plugins.svn.wordpress.org/kento-post-view-counter/tags/2.0@863011 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kento-pvc-admin-stats.php
+++ kento-pvc-admin-stats.php
@@ -238,7 +238,47 @@ if ( $page_links ) {
     <div id="kento-pvc-feedbck-box">
     We are contentiously update this plugin to get more feature, We are waiting to see your reviews and feedback at <a href="http://wordpress.org/plugins/kento-post-view-counter/" >Kento Post View Counter</a><br /><br />
 if you need more help please feel free to join our forum and ask any question<br />
-<a href="http://kentothemes.com/support/" >KentoThemes.com Questions & Answers</a>
+<a href="http://kentothemes.com/support/" >KentoThemes.com Questions & Answers</a><br /><br />
+<strong>Please Share With your Friends</strong><br /><br />
+<table>
+<tr>
+<td width="100px"> 
+<!-- Place this tag in your head or just before your close body tag. -->
+<script type="text/javascript" src="https://apis.google.com/js/platform.js"></script>
+
+<!-- Place this tag where you want the +1 button to render. -->
+<div class="g-plusone" data-size="medium" data-href="http://kentothemes.com/items/plugins/kento-post-view-counter/"></div>
+
+</td>
+<td width="100px">
+
+<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fkentothemes.com%2Fitems%2Fplugins%2Fkento-post-view-counter%2F&amp;width=100&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21&amp;appId=743541755673761" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
+
+ </td>
+<td width="100px"> 
+
+
+
+
+
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://kentothemes.com/items/plugins/kento-post-view-counter/">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+</td>
+
+</tr>
+
+</table>
+
+
+
+
+
+
+<br />
+
+<br />
+
+
 
 
     </div> <!--kento-pvc-feedbck-box -->   
